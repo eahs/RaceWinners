2a158ab28ae7360a8a44e705ff928f71b093aeed
Michael Tanczos <mike@logic-gate.com>
2024-09-11 07:53:35 -0400

    feat: Update codebase to C# 10 and .NET 8.0
    Significant changes include:
    - Updated the target framework from .NET 7.0 to .NET 8.0 in the project file.
    - Refactored all classes to use C# 10's file-scoped namespaces.
    - Simplified class structures by removing unnecessary nesting.
    - Maintained functionality of DataService, Group, and Program classes with no changes in logic or behavior.

--- RaceWinners/RaceWinners/Program.cs
+++ RaceWinners/RaceWinners/Program.cs
@@ -1,24 +1,23 @@
 ﻿using System;
 using System.Threading.Tasks;
 
-namespace RaceWinners
-{
-    class Program
-    {
-        static async Task Main(string[] args)
-        {
-            DataService ds = new DataService();
- 
-            // Asynchronously retrieve the group (class) data
-            var data = await ds.GetGroupRanksAsync();
+namespace RaceWinners;
 
-            for (int i = 0; i < data.Count; i++)
-            {
-                // Combine the ranks to print as a list
-                var ranks = String.Join(", ", data[i].Ranks);
-                
-                Console.WriteLine($"{data[i].Name} - [{ranks}]");
-            }
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        // Asynchronously retrieve the group (class) data
+        var data = await ds.GetGroupRanksAsync();
+
+        for (int i = 0; i < data.Count; i++)
+        {
+            // Combine the ranks to print as a list
+            var ranks = String.Join(", ", data[i].Ranks);
+            
+            Console.WriteLine($"{data[i].Name} - [{ranks}]");
         }
     }
 }
